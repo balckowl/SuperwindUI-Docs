2232470fe9ca72ce24619aeb6f33b735f15b3d52
窪山天望 <shiggyjr1492@gmail.com>
2023-11-29 18:15:34 +0900

    update: OGP画像更新と名前を変更しました。

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Home from "./Home"
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: 'SuoerwindUI',
+  title: 'SuperwindUI',
   description: 'モダンなUIを超速で開発できる次世代のコンポーネントライブラリを提供します。',
   openGraph: {
     title: 'SuperwindUI',
